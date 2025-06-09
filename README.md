398497aef91e494092b64d939b36352101dc79b6
neducod <nedunony16@gmail.com>
2025-06-09 23:42:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# birthday-countdown
+
+
+IMCOMPLETE
---
+++ script.js
@@ -0,0 +1,21 @@
+let userName;
+
+document.getElementById("myButton").onclick = function(){
+    userName = document.getElementById("myText").value;
+    document.getElementById("firstText").textContent = `Hey  ${userName} ,do you want to see how long until your next birthday?`
+}
+
+document.getElementById("myButton").addEventListener("click", function(){
+    document.getElementById("wrapPer").style.display = "block";
+})
+
+document.getElementById("myButton").addEventListener("click", function(){
+    const name = document.getElementById("myText").value.trim();
+
+    if(name == ""){
+        alert("Please enter your name");
+    }
+    else{
+
+    }
+})
