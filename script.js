046e6a63ab24a8d5d2ec0a69e4eb083f7640cfb4
neducod <nedunony16@gmail.com>
2025-06-11 14:11:55 +0100

    adding new features and currently debugging

--- script.js
+++ script.js
@@ -16,6 +16,23 @@ document.getElementById("myButton").addEventListener("click", function(){
             userName = document.getElementById("myText").value;
             document.getElementById("firstText").textContent = `Hey  ${userName} , do you want to see how long until your next birthday?`
         }
-        
+/*
+        document.getElementById("myButton").addEventListener("click", function(){
+            document.getElementById("popup").style.display = "block";
+        })*/
     }
 })
+
+
+/*
+//POP UP CODE
+window.onload = function () {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'flex';
+
+    // Hide after 5 seconds (5000 milliseconds)
+    setTimeout(() => {
+      popup.style.display = 'none';
+    }, 5000);
+  };
+*/
