328fa8ce2b1912dc28ccd14a516875f9ce7021ff
neducod <nedunony16@gmail.com>
2025-06-11 12:36:38 +0100

    Deleted a commented code

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 let userName;
-/*
-document.getElementById("myButton").onclick = function(){
-    userName = document.getElementById("myText").value;
-    document.getElementById("firstText").textContent = `Hey  ${userName} ,do you want to see how long until your next birthday?`
-}
-*/
+
 document.getElementById("myButton").addEventListener("click", function(){
     const name = document.getElementById("myText").value.trim();
 
